e923e17acb8e8c457e754fd957a75fb630d29b2d
Abdulrauf-Remi <anadeenara@gmail.com>
2022-06-09 21:28:52 +0000

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabettin lowwercase
+ */
+
+void print_alphabrtt(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+
+	_putchar('\n');
+}
