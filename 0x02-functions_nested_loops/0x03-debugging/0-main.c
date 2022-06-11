af95410f4b782b69b46a34673a88c106c89e25d2
Abdulrauf-Remi <anadeenara@gmail.com>
2022-06-11 06:26:01 +0000

    Task 0

--- 0x02-functions_nested_loops/0x03-debugging/0-main.c
+++ 0x02-functions_nested_loops/0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main test fuction that prints if integer is positive or negative
+ * main - tests fuction that prints if integer is positive or negative
  *
  * Return: 0
  */
