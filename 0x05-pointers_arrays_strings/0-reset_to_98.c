1fad453aae04075fa173f4ec627bbe901fcb2f6f
Abdulrauf-Remi <anadeenara@gmail.com>
2022-06-14 04:14:47 +0000

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,9 @@
 
 /**
  * reset_to_98 - Update the value a pointer points to.
- * @n the variable the pointer points to.
+ * @n: the variable the pointer points to.
  */
 void reset_to_98(int *n)
 {
-	int *n;
-
 	*n = 98;
 }
