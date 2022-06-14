168c4274e34a899abd5b5eccf53bd6f1415af487
Abdulrauf-Remi <anadeenara@gmail.com>
2022-06-14 05:34:39 +0000

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,12 +7,15 @@
 void revstring(char *s)
 {
 	int len = 0, index = 0;
+	char tmp;
 
 	while (s[index++])
 		len++;
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
-	_putchar('\n');
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		tmp = s[index];
+		s[index] = s[len - index -1];
+		s[len - index - 1] = tmp;
+	}
 }
