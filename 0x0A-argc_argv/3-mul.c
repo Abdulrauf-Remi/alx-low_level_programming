53f1948599133bf12233d9b0867331b850526e81
Abdulrauf-Remi <anadeenara@gmail.com>
2022-07-01 23:34:57 +0000

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Multiplies two numbers
+ * @argc: The number of argument passed.
+ * @argv: The array of argument passed.
+ *
+ * Return: 0 if program gets two argumrnts. 1 if otherwise.
+ */
+int main(int argc, char *argv[])
+{
+	int mul, n1, n2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	mul = n1 * n2;
+
+	printf("d\n", mul);
+
+	return (0);
+}
