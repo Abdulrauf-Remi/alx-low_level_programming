d45af498adb1ff25bfb4d105fccd9242ad18b8e1
Abdulrauf-Remi <anadeenara@gmail.com>
2022-09-01 15:31:08 +0000

    task 1 with header file

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dlistint_s - doulby linked linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for ALX project.
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+/* fuctions */
+
+size_t print_dlistint(const dlistint *h);
+
+#endif
