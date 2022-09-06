fa305a3536be2a750fd6c1f481f3bb8db450875f
Abdulrauf-Remi <anadeenara@gmail.com>
2022-09-06 00:25:35 +0000

    bash_file

---
+++ 0x18-dynamic-librabies/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the length of a string.
+ * @s: The string in question.
+ *
+ * Return: The lenght of *s.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+
+	return (len);
+}
